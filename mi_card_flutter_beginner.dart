02fa7207b2fc40aa175d6ab5664057d61ecf930c
Sandeep N <60995458+sandeepN06@users.noreply.github.com>
2020-10-15 11:23:00 +0530

    ID Card
    ID card having said you can add your photo and your designation by cloning it to your local pc 👍 
    IF you dont have the prescribed font download it from google fonts or you can just use default font
    * Android studio
    * Flutter packages installed

---
+++ mi_card_flutter_beginner.dart
@@ -0,0 +1,101 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              CircleAvatar(
+                radius: 50,
+                // backgroundColor: Colors.red,
+                backgroundImage: AssetImage('-----Your local image----'),
+              ),
+              Text(
+                "------Your Name----",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                '-----YOUR ROLE------',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal[100],
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal[100],
+                  ),
+              ),
+              Card(
+                // padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+
+                  title: Text(
+                    '+91 789 2800 801',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+
+              ),
+              Card(
+                color: Colors.white,
+                // padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+
+                    ),
+
+                    title: Text(
+                      "sandeepbk01@gmail.com",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal[900],
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+
+        ),
+      ),
+    );
+  }
+}
+
+
